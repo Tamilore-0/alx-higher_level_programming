1872d8e1e50d4fdb1b6c234602526895fa0a98cf
Tamilore <findtamilore@gmail.com>
2023-10-31 23:46:46 +0100

    task 5 final correction

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for num in range(0, 101):
-        if num % 3 == 0:
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 != 0:
             print("Fizz", end=" ")
-        elif num % 5 == 0:
+        elif num % 5 == 0 and num % 3 != 0:
             print("Buzz", end=" ")
-        elif num % 5 and 3:
+        elif num % 5 == 0 and 3 == 0:
             print("FizzBuzz", end=" ")
+        else:
+            print("{:d}".format(num), end = " ")
+            
+fizzbuzz()
+print(" ")
