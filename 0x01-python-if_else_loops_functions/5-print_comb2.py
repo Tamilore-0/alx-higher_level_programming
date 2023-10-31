89db48e011c53d1b75f0c998606e6c7eec4ef779
Tamilore <findtamilore@gmail.com>
2023-10-31 23:35:58 +0100

    task 5 correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 # prints numbers in double digits
-for x in range(0, 10):
-    for y in range(0, 10):
-        print("{}{}, ".format(x, y), end="")
+for number in range(0, 100):
+    print("{:02d}".format(number), end="")
+    if number < 99:
+        print(", ", end="")
 # new line
 print()
