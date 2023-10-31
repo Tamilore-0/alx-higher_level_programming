6b46067a11d253d2dd5c020a5618899c74d09294
Tamilore <findtamilore@gmail.com>
2023-10-31 21:13:43 +0100

    Updated files

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 for x in range(0, 10):
-    for y in range (x, 10):
+    for y in range(x, 10):
+        # ensures numbers are distinct
         if y == x:
             continue
-        print("{}{}, ".format(x, y), end='')
+        print("{}{}".format(x, y), end='')
+        # ensures comma is not printed at the end
+        if x < 8:
+            print(", ", end="")
 # new line
 print()
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def islower(c):
+    for value in range(ord('a'), ord('z') + 1):
+        if ord(c) == value:
+            return True
+        else:
+            continue
+
+
+print()
