61edc8c9ed58604389c2ed5abed480f011082ea4
Tamilore <findtamilore@gmail.com>
2023-10-31 21:59:07 +0100

    Added files

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,8 +2,8 @@
 def fizzbuzz():
     for num in range(0, 101):
         if num % 3 == 0:
-            print("Fizz ")
+            print("Fizz", end=" ")
         elif num % 5 == 0:
-            print("Buzz ")
+            print("Buzz", end=" ")
         elif num % 5 and 3:
-            print("FizzBuzz ")
+            print("FizzBuzz", end=" ")
---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+* insert_node - Function that inserts a node
+* @head: A pointer to a pointert to the head of the list
+* @number: The value of the new node to be inserted
+* Return: listint_t
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *current = *head;
+	listint_t *node, *temp;
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+	{
+		return (NULL);
+	}
+	node->n = number;
+	node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = node;
+		return (node);
+	}
+
+	while (current->next != NULL)
+	{
+		if (current->next->n > number)
+		{
+			temp = current->next;
+			current->next = node;
+			node->next = temp;
+		}
+
+		current = current->next;
+	}
+
+	return (node);
+}
