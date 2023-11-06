ffd630f5dbbb7c7a57bcb534a90e216a912e75e2
Tamilore <findtamilore@gmail.com>
2023-11-06 12:10:01 +0100

    Task 11 corrected

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -10,10 +10,3 @@ def delete_at(my_list=[], idx=0):
             continue
         i += 1
     return my_list
-    
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_list = delete_at(my_list, idx)
-print(new_list)
-print(my_list)
