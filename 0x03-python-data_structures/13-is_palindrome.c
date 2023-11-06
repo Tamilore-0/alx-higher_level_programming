a8dc90d138c9e3e640e9ef09eac0ac7f3c2056a2
Tamilore <findtamilore@gmail.com>
2023-11-06 13:46:45 +0100

    updated files

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,14 +1,18 @@
 #include "lists.h"
 
 /**
-* is_palindrome - entry point
-* @head: a double pointer that points to a linked list
-* Return: 0 if it is not a palindrome, 1 if it is a palindrome
-*/
+ * is_palindrome - entry point
+ * @head: a double pointer that points to a linked list
+ * Return: 0 if it is not a palindrome, 1 if it is a palindrome
+ */
 int is_palindrome(listint_t **head)
 {
 	listint_s *current = *head;
 	int rev_num;
+
+	int remainder = 0;
+
+	int num;
 
 	if (*head == NULL)
 	{
@@ -17,31 +21,17 @@ int is_palindrome(listint_t **head)
 
 	while (current != NULL)
 	{
-		rev_num = reverseInteger(current->n);
+		rev_num = 0;
+		num = current->n;
+		while (num != 0)
+		{
+			remainder = num % 10;
+			rev_num = rev_num * 10 + remainder;
+			num /= 10;
+		}
 		if (current->n != rev_num)
 			return (0);
 		current = current->next;
 	}
 	return (1);
-}
-
-/**
-* reverseInteger - Entry point
-* @num: Number to be reversed
-* Return: reversed number
-*/
-int reverseInteger(int num)
-{
-
-	int reversed = 0;
-
-	while (num != 0)
-	{
-		int remainder = num % 10;
-
-		reversed = reversed * 10 + remainder;
-		num /= 10;
-	}
-
-	return (reversed);
 }
