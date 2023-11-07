f85dc78938361e28193499c59fe9807ba195ac51
Tamilore <findtamilore@gmail.com>
2023-11-07 13:28:18 +0100

    Updated tasks

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,37 +1,69 @@
 #include "lists.h"
 
 /**
- * is_palindrome - entry point
- * @head: a double pointer that points to a linked list
- * Return: 0 if it is not a palindrome, 1 if it is a palindrome
- */
+* reverse_listint - Reverses a singly-linked listint_t list.
+* @head: A pointer to the starting node of the list to reverse.
+*
+* Return: A pointer to the head of the reversed list.
+*/
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *node = *head, *next, *prev = NULL;
+
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
+
+	*head = prev;
+	return (*head);
+}
+
+/**
+* is_palindrome - Checks if a singly linked list is a palindrome.
+* @head: A pointer to the head of the linked list.
+*
+* Return: If the linked list is not a palindrome - 0.
+*         If the linked list is a palindrome - 1.
+*/
 int is_palindrome(listint_t **head)
 {
-	listint_s *current = *head;
-	int rev_num;
+	listint_t *tmp, *rev, *mid;
+	size_t size = 0, i;
 
-	int remainder = 0;
-
-	int num;
-
-	if (*head == NULL)
-	{
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
+
+	tmp = *head;
+	while (tmp)
+	{
+		size++;
+		tmp = tmp->next;
 	}
 
-	while (current != NULL)
+	tmp = *head;
+	for (i = 0; i < (size / 2) - 1; i++)
+		tmp = tmp->next;
+
+	if ((size % 2) == 0 && tmp->n != tmp->next->n)
+		return (0);
+
+	tmp = tmp->next->next;
+	rev = reverse_listint(&tmp);
+	mid = rev;
+
+	tmp = *head;
+	while (rev)
 	{
-		rev_num = 0;
-		num = current->n;
-		while (num != 0)
-		{
-			remainder = num % 10;
-			rev_num = rev_num * 10 + remainder;
-			num /= 10;
-		}
-		if (current->n != rev_num)
+		if (tmp->n != rev->n)
 			return (0);
-		current = current->next;
+		tmp = tmp->next;
+		rev = rev->next;
 	}
+	reverse_listint(&mid);
+
 	return (1);
 }
