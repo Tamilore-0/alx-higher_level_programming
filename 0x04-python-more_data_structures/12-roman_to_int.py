5d29c00ad258de6180a03b78a53f12b6d63d1bb2
Tamilore <findtamilore@gmail.com>
2023-11-08 21:37:24 +0100

    Task 12 correction

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
-        return None
+        return 0
     my_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     sum = 0
     roman_string += 'Z'
+    string = "IVXLCDM"
     for i in range(len(roman_string) - 1):
         current = roman_string[i]
         forward = roman_string[i + 1]
+        
+        for j in string:
+            if string.count(current) == 0:
+                return 0
+ 
         for key, value in my_dict.items():
             if current == key:
                 if current == roman_string[-2]:
@@ -17,6 +23,4 @@ def roman_to_int(roman_string):
                     sum += my_dict[key]
                 else:
                     sum -= my_dict[key]
-            else:
-                continue
     return abs(sum)
