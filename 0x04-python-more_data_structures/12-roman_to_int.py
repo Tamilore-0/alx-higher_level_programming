4276cf8d1667a6d6159119b2888b02e02c927cba
Tamilore <findtamilore@gmail.com>
2023-11-08 20:59:08 +0100

    Task 12 upload

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -18,16 +18,3 @@ def roman_to_int(roman_string):
                 else:
                     sum -= my_dict[key]
     return abs(sum)
-roman_number = "X"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-roman_number = "VII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "MMMIXCXV"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "LXXXVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-roman_number = "DCCVII"
-print("{} = {}".format(roman_number, roman_to_int(roman_number)))
