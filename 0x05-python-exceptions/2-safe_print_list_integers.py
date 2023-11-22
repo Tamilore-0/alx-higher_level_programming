66922f770753dbfc4e04fc822989031077f6edf7
Tamilore <findtamilore@gmail.com>
2023-11-23 00:32:54 +0100

    update

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,32 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-
     for i in range(x):
         try:
-            # Try to print the integer value of my_list[i] with formatting
             print("{:d}".format(my_list[i]), end="")
-            count += 1  # Increment count if successful
-        except (ValueError, TypeError):
-            continue  # Skip to the next iteration if the element is not an integer
-
-    print()  # Print a newline after printing the integers
-    return count 
-
-
-
-
-
-
-
-my_list = [1, 2, 3, 4, 5]
-
-nb_print = safe_print_list_integers(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-
-my_list = [1, 2, 3, "School", 4, 5, [1, 2, 3]]
-nb_print = safe_print_list_integers(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-
-nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))     
+            count = count + 1
+        except (TypeError, ValueError):
+            pass
+    print("")
+    return count
