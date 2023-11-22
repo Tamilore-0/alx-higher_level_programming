f77b5836b8681f6122dd3701454f1eee80367801
Tamilore <findtamilore@gmail.com>
2023-11-22 19:07:51 +0100

    task 0, 1

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
+
     for i in range(x):
-        if my_list[i].isdigit() != 0:
-            continue
         try:
-            print(f"{my_list[i]}", sep="", end="")
-        except IndexError:
-            break
-        else:
-            count += 1
-    print()
-    return count
+            # Try to print the integer value of my_list[i] with formatting
+            print("{:d}".format(my_list[i]), end="")
+            count += 1  # Increment count if successful
+        except (ValueError, TypeError):
+            continue  # Skip to the next iteration if the element is not an integer
+
+    print()  # Print a newline after printing the integers
+    return count 
+
+
+
+
+
+
 
 my_list = [1, 2, 3, 4, 5]
 
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+
+        Parameters:
+        - size: The size of the square.
+        """
+        self.__size = size
