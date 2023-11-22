87cb0d0bc3ee84f6cbb12543fdc294626d262834
Tamilore <findtamilore@gmail.com>
2023-11-22 19:15:45 +0100

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""
+A module that defines a square class
+"""
+
+
 class Square:
     def __init__(self, size):
         """
         Initializes a new Square instance.
-
         Parameters:
         - size: The size of the square.
         """
