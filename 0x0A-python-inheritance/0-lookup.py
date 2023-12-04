3f9ab615d3f2e4655520095cd322f6d50e44926f
Tamilore <findtamilore@gmail.com>
2023-12-04 13:02:52 +0100

    task 0 upload

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that defines a lookup function
+"""
+
+
+def lookup(obj):
+    """
+    function that returns the list of available attributes and methods of an object
+    """
+    return dir(obj)
