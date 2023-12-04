c37396bd324bfc2264de11496623d127924de0d2
Tamilore <findtamilore@gmail.com>
2023-12-04 15:51:13 +0100

    task 100 upload

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,22 @@
 #!/usr/bin/python3
 """
-
+Module defining the MyInt class.
 """
 
 
 class MyInt(int):
+    """
+    MyInt class that inherits from int with inverted equality operators.
+    """
+
     def __eq__(self, other):
+        """
+        Override the equality operator (==), inverting the comparison.
+        """
         return super().__ne__(other)
 
     def __ne__(self, other):
+        """
+        Override the inequality operator (!=), inverting the comparison.
+        """
         return super().__eq__(other)
