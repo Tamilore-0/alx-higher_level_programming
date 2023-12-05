88c002c90f8bedc56798cc2588e7ecb35217021c
Tamilore <findtamilore@gmail.com>
2023-12-05 16:52:36 +0100

    task 7 upload

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,8 +5,24 @@
 
 
 import sys
+import os
 save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
+filename = "add_item.txt"
 
-lists = []
+if not os.path.exists(filename):
+    mode = 'w'
+else:
+    mode = 'a'        
+                                  
+with open(filename, mode) as file:
+    for arg in sys.argv[1:]:
+        file.write(f"{arg}\n")
+
+with open(filename, 'r') as file1:
+    lists = file1.read().splitlines()
+
+save(lists, "add_item.json")
+content = load("add_item.json")
+print(f"{content}")
