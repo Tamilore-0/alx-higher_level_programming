b8daf32d6f05b997d737e01bdc87f5a6755935c5
Tamilore <findtamilore@gmail.com>
2023-12-06 15:40:18 +0100

    update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,4 +5,14 @@ module
 
 
 def class_to_json(obj):
-    
+    """Returns the dictionary description with simple
+    data structure (list, dict, str, int and bool) for JSON
+    serialization
+
+    Args:
+        obj (object): Object to serialize
+
+    Returns:
+        dict: The dictionary description
+    """
+    return obj.__dict__
