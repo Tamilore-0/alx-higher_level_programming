61e912af3368b20dce840e908a1680355665b6b6
Tamilore <findtamilore@gmail.com>
2023-12-06 16:11:24 +0100

    11 update

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Module
+"""
+
+
+class Student:
+    """
+    class that defines a student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) is list:
+            if all(isinstance(_, str) for _ in attrs):
+                selected_attrs = {attr: getattr(self, attr)
+                                  for attr in attrs if hasattr(self, attr)}
+                return selected_attrs
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        new_dict = {key: value for key, value in json.items()}
+        return new_dict
