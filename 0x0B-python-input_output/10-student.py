8b140c47a7c9ce5fb419fb3d7ce6083776f927fb
Tamilore <findtamilore@gmail.com>
2023-12-06 15:57:49 +0100

    10 update

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -16,6 +16,7 @@ class Student:
     def to_json(self, attrs=None):
         if type(attrs) is list:
             if all(isinstance(_, str) for _ in attrs):
-                selected_attrs = {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+                selected_attrs = {attr: getattr(self, attr)
+                                  for attr in attrs if hasattr(self, attr)}
                 return selected_attrs
         return self.__dict__
