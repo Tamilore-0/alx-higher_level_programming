e0670688a797b8b03286cf096209b7942611dda6
ayo <findtamilore@gmail.com>
2024-01-16 21:28:04 +0100

    fix syntax

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,4 @@
 -- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
-SOURCE temperatures.sql;
 SELECT city, avg(value) as avg_temp
 FROM temperatures
 GROUP BY city
