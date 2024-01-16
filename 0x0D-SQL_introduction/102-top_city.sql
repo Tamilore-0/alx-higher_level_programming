a6df1da1f06e583421add12547ee866e6891a408
ayo <findtamilore@gmail.com>
2024-01-16 21:41:19 +0100

    fixed

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,6 +2,7 @@
 -- and August ordered by temperature (descending)
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
+WHERE `month` = 7 OR `month` = 8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
