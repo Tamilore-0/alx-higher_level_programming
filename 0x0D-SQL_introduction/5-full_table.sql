d4d3d81355a5900e7409cc069023ac4e2f2ca334
ayo <findtamilore@gmail.com>
2024-01-16 20:38:17 +0100

    update 3

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,3 @@
--- script that prints the full description of the table first_table from the database hbtn_0c_0 in your MySQL server.
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'first_table';
+-- script that prints the full description of the table first_table
+-- from the database hbtn_0c_0 in your MySQL server.
+SHOW COLUMNS FROM first_table;
