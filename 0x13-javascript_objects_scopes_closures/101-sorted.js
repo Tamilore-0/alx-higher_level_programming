57dca29fd897c2840ef8bee08e71c4f71cf8ee73
ayo <findtamilore@gmail.com>
2024-02-14 11:30:19 +0100

    update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,12 @@
 #!/usr/bin/node
 
 const dict = require('./101-data').dict;
-
 const newDict = {};
-const valuesArray = Object.values(dict);
 
 for (const key in dict) {
   // check if ley already exists
   if (dict[key] in newDict) {
-      newDict[dict[key]].push(key);
+    newDict[dict[key]].push(key);
   } else {
     newDict[dict[key]] = [key];
   }
