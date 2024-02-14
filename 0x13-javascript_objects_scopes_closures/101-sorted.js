7780c8a32fa3db773438026563bbb883437561c1
ayo <findtamilore@gmail.com>
2024-02-14 11:28:30 +0100

    update

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -6,12 +6,9 @@ const newDict = {};
 const valuesArray = Object.values(dict);
 
 for (const key in dict) {
-  //check if ley already exists
+  // check if ley already exists
   if (dict[key] in newDict) {
-    //check if value already exists
-    if (!(key in valuesArray)) {
       newDict[dict[key]].push(key);
-    }
   } else {
     newDict[dict[key]] = [key];
   }
--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,12 +2,10 @@
 const Square1 = require('./5-square');
 
 class Square extends Square1 {
-
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
       this.print();
-    }
-    else {
+    } else {
       for (let i = 0; i < this.height; i++) {
         let row = '';
         for (let j = 0; j < this.width; j++) {
