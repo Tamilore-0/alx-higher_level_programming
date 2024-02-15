1a36b7a33220dac9efe9f9bb2cf7b069bc018bbb
ayo <findtamilore@gmail.com>
2024-02-15 22:22:03 +0100

    creates task 1 & 2 scripts

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+A script that takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+import sys
+
+
+def main():
+    """
+    main function that displays all values in the states
+    table of hbtn_0e_0_usa where name matches the argument.
+    """
+
+    # store connection details
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    name_to_search = sys.argv[4]
+
+    # Establish a connection with the MySQL databse
+    conn = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database_name
+    )
+
+    # Create a cursor object
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM states WHERE name = %s', (name_to_search,))
+
+    # Fetch records from the result set
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == '__main__':
+    main()
