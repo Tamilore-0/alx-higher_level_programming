80c9d90b796393a03a15be991527367adcf40f6f
ayo <findtamilore@gmail.com>
2024-02-16 13:36:50 +0100

    task 5 done

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -30,29 +30,20 @@ def main():
     # Create a cursor object
     cursor = conn.cursor()
 
-    # Get the id of specified state
-    cursor.execute(
-        'SELECT id FROM states WHERE name LIKE BINARY %s', (state_name,)
-    )
-    states_id = cursor.fetchone()
+    query = 'SELECT name FROM cities \
+        WHERE state_id = (SELECT id FROM states WHERE name = %s) \
+        ORDER BY id ASC'
 
-    # Find cities under the same id
-    if states_id:
-        cursor.execute(
-            'SELECT name FROM cities WHERE state_id = %s', (states_id,)
-        )
-        records = cursor.fetchall()
-        if records:
-            for row in records:
-                if (row != records[0]):
-                    print(', ', end='')
-                print(row[0], end='')
+    # Execute the query with the state name as parameter
+    cursor.execute(query, (state_name,))
+    # Print results
+    records = cursor.fetchall()
+    if records:
+        for row in records:
+            if (row != records[0]):
+                print(', ', end='')
+            print(row[0], end='')
     print()
-
-    # Fetch records from result set
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
 
 
 if __name__ == '__main__':
