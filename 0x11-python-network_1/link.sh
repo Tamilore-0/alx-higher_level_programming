05b5cdfc4e0e2b5579f5a1d398e0832c1ec8f793
tami-cp0 <findtamilore>
2024-03-01 17:51:22 +0100

    remove test file

---
+++ 0x11-python-network_1/link.sh
@@ -0,0 +1,38 @@
+
+    # sudo apt-get -y install expect 
+
+    # git config --global user.email findtamilore
+    # git config --global user.name tami-cp0
+    # git config --global credential.helper store
+
+    username=$"$1"
+    repository=$"$2"
+    personal_token=$"$3"
+
+    git clone "https://github.com/$username/$repository.git" && wait
+    touch "$repository/test" && echo "test file created"
+
+    # function push() {
+    #     git add "$1" && git commit -a -m "$2" && git push
+    # }
+
+    
+    cd "$repository" && wait
+    # Run expect script
+expect << SCRIPT
+    spawn bash
+    expect "$ "
+    send "push 'test' 'testing'\r"
+    expect "Username:"
+    send "$username\r"
+    expect "Password:"
+    send "$personal_token\r"
+    expect "$ "
+    send "exit\r"
+    expect eof
+SCRIPT
+
+    rm "$2/test"
+    push "$2/test" "removing test"
+
+    echo "\n[100%] Git Setup Complete..."
