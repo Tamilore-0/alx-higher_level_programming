3f0e2c7f9882a0c55dcd93a238202537937672d9
tami-cp0 <findtamilore>
2024-03-01 18:14:34 +0100

    .

--- 0x11-python-network_1/link.sh
+++ 0x11-python-network_1/link.sh
@@ -1,9 +1,9 @@
 
-    # sudo apt-get -y install expect 
+    sudo apt-get -y install expect
 
-    # git config --global user.email findtamilore
-    # git config --global user.name tami-cp0
-    # git config --global credential.helper store
+    git config --global user.email findtamilore
+    git config --global user.name tami-cp0
+    git config --global credential.helper store
 
     username=$"$1"
     repository=$"$2"
@@ -12,9 +12,9 @@
     git clone "https://github.com/$username/$repository.git" && wait
     touch "$repository/test" && echo "test file created"
 
-    # function push() {
-    #     git add "$1" && git commit -a -m "$2" && git push
-    # }
+    function push() {
+        git add "$1" && git commit -a -m "$2" && git push
+    }
 
     
     cd "$repository" && wait
@@ -28,11 +28,9 @@ expect << SCRIPT
     expect "Password:"
     send "$personal_token\r"
     expect "$ "
+    send "rm test\r"
+    send "push 'test' 'remove test file'\r"
     send "exit\r"
     expect eof
 SCRIPT
-
-    rm "$2/test"
-    push "$2/test" "removing test"
-
     echo "\n[100%] Git Setup Complete..."
