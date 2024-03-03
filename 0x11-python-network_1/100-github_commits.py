cc57e4d622c224cf9ec9ef7700d43da4cbed790d
tami-cp0 <findtamilore>
2024-03-03 07:00:10 -0800

    syntax change

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """
+Script that gets the last 10 commits of a specified user
 """
 import requests
 import sys
 
 if __name__ == "__main__":
-    repo = sys.argv[1]
-    owner = sys.argv[2]
+    repository_name = sys.argv[1]
+    owner_name = sys.argv[2]
 
-    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+    url = f"https://api.github.com/repos/{owner_name}/{repository_name}/commits"
     response = requests.get(url)
     json_data = response.json()
     i = 0
 
     while i < 10:
-        commit_id = f"{json_data[i].get('sha', '')}"
-        committer = f"{json_data[i]['commit']['author']['name']}"
+        commit_id = json_data[i]['sha']
+        committer = json_data[i]['commit']['author']['name']
         print(f"{commit_id}: {committer}")
         i = i + 1
