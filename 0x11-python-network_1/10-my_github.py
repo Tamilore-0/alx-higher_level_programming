c887c2503f0fc84c473246b0257a224405a5f837
tami-cp0 <findtamilore>
2024-03-03 04:57:34 -0800

    done

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Python script that takes your GitHub credentials
+(username and password) and uses the GitHub API to display your id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    data = {"name": username} 
+    headers = {"Authorization": f"Bearer {password}"}
+    response = requests.get("https://api.github.com/user", data=data, headers=headers)
+
+    try:
+        json_data = response.json()
+        print(f"{json_data.get('id', 'None')}")
+    except Exception as e:
+        print("JSON decoding failed")
