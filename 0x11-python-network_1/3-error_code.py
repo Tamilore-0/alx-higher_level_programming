4f4ca81b90b4f2e57d2cf6ce1b9f6d6138d28974
tami-cp0 <findtamilore>
2024-03-03 02:29:28 -0800

    tab indent

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the 
+Python script that takes in a URL, sends a request to the
 URL and displays the body of the response (decoded in utf-8).
 """
 import urllib.request
 import sys
 import urllib.error
 
-url = sys.argv[1]
+if __name__ == "__main__":
+    url = sys.argv[1]
 
-req = urllib.request.Request(url)
-try:
-	with urllib.request.urlopen(req) as response:
-		content = response.read().decode('utf-8')
-		print(content)
-except urllib.error.HTTPError as e:
-	print(f"Error code: {e.code}")
+    req = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(req) as response:
+            content = response.read().decode('utf-8')
+            print(content)
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
