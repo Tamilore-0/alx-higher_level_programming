f616bdbaed8da845ecc00c3522f7dcf77cda1678
tami-cp0 <findtamilore>
2024-03-27 02:59:51 +0100

    final upload v2

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,19 +1,31 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
+
+/**
+ * script that prints all characters of a Star Wars movie
+ * in the right order
+ */
+
+if (process.argv.length > 2) {
+  const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}/`;
+  const request = require('request');
+
+  request(url, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
     }
+    const jsonData = JSON.parse(body);
+
+    jsonData.characters.forEach(element => {
+      request(element, (error, response, body) => {
+        if (error) {
+          console.error('Error:', error);
+          return;
+        }
+
+        const charData = JSON.parse(body);
+        console.log(charData.name);
+      });
+    });
   });
 }
